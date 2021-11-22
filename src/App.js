472641218cb73eee50013adafbe4c27a3a9b5c48
JanGolabek <janekgolabekk@hotmail.com>
2021-11-22 14:28:06 +0100

    Desktop version done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import "./index.css";
 import './App.css';
-
+import React, {useState} from "react";
+ 
 function App() {
+  const [Name, setDataName]=useState(null);
+  const [Street, setDataStreet]=useState(null);
+  const [Zip, setDataZip]=useState(null);
+  const [City, setDataCity]=useState(null);
+ 
+ 
+  function getDataName(val){
+    setDataName(val.target.value)
+  }
+ 
+  function getDataStreet(val){
+    setDataStreet(val.target.value)
+  }
+ 
+  function getDataZip(val){
+    setDataZip(val.target.value)
+  }
+ 
+  function getDataCity(val){
+    setDataCity(val.target.value)
+  }
+ 
   return (
-    <div className="App">
+<div className="App">
       <div class="grid-container">
         <div id="chooseMe">
           <h1>Choose an option</h1>
@@ -12,9 +35,9 @@ function App() {
         <img id="pickup" src="/img/map-pin.svg" alt="map-pin"/>
           <h2>I'm picking it up myself</h2>
           <h3>0kr</h3>
-          
+         
         </div>
-
+ 
         <div id="delivery-option2">
         <img id="truck" src="/img/truck.svg" alt="delivery-truck"/>
           <h2>Home delivery</h2>
@@ -23,38 +46,38 @@ function App() {
         <div id="button1">
         <button id="buttonsRed">Back to shoppingcart</button>
         </div>
-
-        
-
+ 
+       
+ 
       <div id="delivery">
      <h2 id="deliveryID">Delivery address</h2>
      <p>Name:
-     <input id="nameInput" type="text"></input>
+     <input id="nameInput" type="text" onChange={getDataName}></input>
      </p>
      
-
+ 
      <p>Street and housenumber:
-     <input id="streetInput" type="text"></input>
+     <input id="streetInput" type="text" onChange={getDataStreet}></input>
      </p>
      
-
+ 
      <p>Zip code:
-     <input id="zipInput" type="text"></input>
+     <input id="zipInput" type="text" onChange={getDataZip}></input>
      </p>
      
-
+ 
      <p>City:
-     <input id="cityInput" type="text"></input>
+     <input id="cityInput" type="text" onChange={getDataCity}></input>
      </p>
-
+ 
      
-     
-
+     <button id="delivery-button">Back To Delivery Options</button>
+ 
    </div>
-   <div id="button2">
-       <button id="buttonsRed">Back To Delivery Options</button>
-     </div>
-
+   
+       
+     
+ 
    <div id="payment">
      <h2 id="payment-id">Payment</h2>
      <h3 id="payment-method">Payment methods:</h3>
@@ -71,36 +94,36 @@ function App() {
        Paypal:
      <img id="payment-picture3" src="/img/paypal.png" width="40px" height="20px" alt="paypal"/>
      </p>
-
+     <button id="buttonsRed">Back to delivery</button>
      
  </div>
- <div id="button3">
-   <button id="buttonsRed">Back to delivery</button>
- </div>
+ 
+   
+ 
  
  <div id="delivery-info1">
    <h3>Total amount: 100kr</h3>
    <h3>Delivery address</h3>
-   <p>Name: *</p>
-   <p>Street and housenumber: *</p>
-   <p>Zip code: *</p>
-   <p>City: *</p>
+   <p>Name: {Name}</p>
+   <p>Street and housenumber: {Street}</p>
+   <p>Zip code: {Zip}</p>
+   <p>City: {City}</p>
    <button id="buttons">Payment</button>
  </div>
-
+ 
  <div id="delivery-info2">
    <h3>Total amount: 100kr</h3>
    <h3>Delivery address</h3>
-   <p>Name: *</p>
-   <p>Street and housenumber: *</p>
-   <p>Zip code: *</p>
-   <p>City: *</p>
+   <p>Name: {Name}</p>
+   <p>Street and housenumber: {Street}</p>
+   <p>Zip code: {Zip}</p>
+   <p>City: {City}</p>
    <button id="buttons">Complete checkout</button>
  </div>
  </div>
-
+ 
     </div>
   );
 }
-
+ 
 export default App;
